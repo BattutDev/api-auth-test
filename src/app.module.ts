b0cc0297699897a7a0258f59670d7777eb15a6fb
BattutDev <antoine@battut.dev>
2023-04-04 17:46:19 +0200

    feat: postgres database support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
+import * as NestConfig from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    NestConfig.ConfigModule.forRoot({ envFilePath: '.env' }),
+    UsersModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
