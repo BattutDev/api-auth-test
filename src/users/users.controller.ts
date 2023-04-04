a2813cfe418a158dcdd3faf8276d8ae82fb05834
BattutDev <antoine@battut.dev>
2023-04-04 22:11:22 +0200

    feat: Add role guards for routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,24 @@
-import { Controller, Get, UnauthorizedException } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
 import User from '../entities/user.entity';
 import { UsersService } from './users.service';
 import { Session } from 'express-session';
 import { Session as GetSession } from '@nestjs/common/decorators/http/route-params.decorator';
 import { UserSession } from '../app.controller';
+import { RolesGuard } from '../roles/roles.guard';
+import { Roles } from '../roles/roles.decorator';
 
 @Controller('users')
+@UseGuards(RolesGuard)
 export class UsersController {
   constructor(private readonly service: UsersService) {}
 
   @Get('/@me')
+  @Roles('root', 'user')
   getMyUserInfo(@GetSession() session: UserSession): Promise<User> {
     return this.service.getById(session.user.id);
   }
