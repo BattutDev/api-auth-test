4c936dbbc1b2ba427d0fa3b8ec16bf319d03e524
BattutDev <antoine@battut.dev>
2023-04-04 14:43:34 +0200

    feat: Auth & Redis store for sessions

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Session as GetSession,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Session } from 'express-session';
+
+type UserSession = Session & Record<'user', any>;
 
 @Controller()
 export class AppController {
-  @Get()
-  getHello(): string {
-    return 'Hello';
+  @Get('me')
+  getMe(@GetSession() session: UserSession) {
+    if (!session.user) throw new UnauthorizedException('Not authenticated');
+    return session.user;
+  }
+  @Post('auth')
+  auth(@GetSession() session: UserSession) {
+    session.user = {
+      email: 'antoine@battut.dev',
+    };
+    return 'auth successful';
+  }
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Post('logout')
+  logout(@GetSession() session: UserSession) {
+    if (!session.user) throw new UnauthorizedException('Not authenticated');
+    return new Promise((resolve, reject) => {
+      session.destroy((err) => {
+        if (err) reject(err);
+        resolve(undefined);
+      });
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import RedisStore from 'connect-redis';
+import * as session from 'express-session';
+import { createClient } from 'redis';
 import { AppModule } from './app.module';
 
+// Initialize client.
+const redisClient = createClient();
+redisClient.connect().catch(console.error);
+
+// Initialize store.
+// @ts-ignore
+const redisStore = new RedisStore({
+  // @ts-ignore
+  client: redisClient,
+  prefix: 'naat', // naat for nest api auth test
+});
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // we add sessions middleware
+  app.use(
+    session({
+      store: redisStore,
+      secret: 'super-secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
